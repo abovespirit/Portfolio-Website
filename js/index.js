27674c8453c4e63fa0e2d1cf76e8db0c84a09489
abovespirit <crc91118@gmail.com>
2023-07-06 13:07:13 -0400

    Update
    Fixes to mobile and small visual changes

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
+const hamburger = document.querySelector(".nav-toggle")
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
+    // hamburger.style.transform = 'translateX(-35em)';
 });
 
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
+        // hamburger.style.transform = 'translateX(33em)';
     })
 })
 
